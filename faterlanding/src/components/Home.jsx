2ebb4189cee3ac6f7002c93d220aa4294679da30
Shayan <87204203+shayanafkane@users.noreply.github.com>
2022-02-08 10:54:15 +0330

    AddSwiper

--- faterlanding/src/components/Home.jsx
+++ faterlanding/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import Nav from './Nav';
+import Slider1 from './Slider1';
 export default class Home extends Component {
   render() {
     return (
       <div>
         <Nav />
+        <Slider1 />
       </div>
     );
   }
---
+++ faterlanding/src/components/Slider1.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "./assets/styles/Slider.css";
+export default class Slider1 extends Component {
+    render() {
+        return (
+            <section className='SliderBox'>
+                <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>Slide 1</SwiperSlide>
+                    <SwiperSlide>Slide 2</SwiperSlide>
+                    <SwiperSlide>Slide 3</SwiperSlide>
+                    <SwiperSlide>Slide 4</SwiperSlide>
+                    <SwiperSlide>Slide 5</SwiperSlide>
+                    <SwiperSlide>Slide 6</SwiperSlide>
+                    <SwiperSlide>Slide 7</SwiperSlide>
+                    <SwiperSlide>Slide 8</SwiperSlide>
+                    <SwiperSlide>Slide 9</SwiperSlide>
+                </Swiper>
+            </section>
+        );
+    }
+}
